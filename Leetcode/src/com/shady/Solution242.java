9e9e47a41beb988d31c4e4221f75d5cb4a29b078
Ray Liu <wooshadyyy@gmail.com>
2022-09-13 16:40:02 -0400

    TEST

--- Leetcode/src/com/shady/Solution242.java
+++ Leetcode/src/com/shady/Solution242.java
@@ -1,4 +1,30 @@
 package com.shady;
 
+import java.util.Arrays;
+//anagram
+//无改动
+
 public class Solution242 {
+    public boolean isAnagram(String s, String t) {
+        char [] a = s.toCharArray();
+        char [] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(Arrays.compare(a,b) == 0){
+            return true;
+        }else{
+            return false;
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        String a1 = "anagram";
+        String a2=  "ramgaan";
+        Solution242 solution242 = new Solution242();
+        System.out.println(solution242.isAnagram(a1,a2));
+    }
 }
