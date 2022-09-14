18a96a3168ff758ba18b122e25cb0b4cc3b6b358
Ray Liu <wooshadyyy@gmail.com>
2022-09-14 14:17:47 -0400

    Leetcode TopKfrequent常规解法

--- Leetcode/src/com/shady/TopKFrequent.java
+++ Leetcode/src/com/shady/TopKFrequent.java
@@ -51,6 +51,31 @@ public class TopKFrequent {
             return null;
     }
 
+
+        /**
+         * Time Complexity: O(nlog(k))
+         * Space Complexity: O(n)
+         */
+        //leetcode solution 1
+        public int[] topKFrequent1(int[] nums, int k) {
+            int[] arr = new int[k];
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+            PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+                    (a, b) ->
+                            a.getValue() - b.getValue()
+            );
+            for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+                pq.add(it);
+                if (pq.size() > k) pq.poll();
+            }
+            int i = k;
+            while (!pq.isEmpty()) {
+                arr[--i] = pq.poll().getKey();
+            }
+            return arr;
+        }
+
     public static void main(String[] args) {
         TopKFrequent topKFrequent =  new TopKFrequent();
         int nums [] = new int[]{1,1,1,2,2,3};
