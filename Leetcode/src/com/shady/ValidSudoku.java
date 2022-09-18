60c79fdd5b014ca7a788a4092ced005673de9b5c
Ray Liu <wooshadyyy@gmail.com>
2022-09-18 15:41:56 -0400

    valid sudoku问题, 用Hashset数组来分别存储 row, column, box中的值

---
+++ Leetcode/src/com/shady/ValidSudoku.java
@@ -0,0 +1,96 @@
+package com.shady;
+
+import java.util.HashSet;
+
+//Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+//
+//        Each row must contain the digits 1-9 without repetition.
+//        Each column must contain the digits 1-9 without repetition.
+//        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+//        Note:
+//
+//        A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+//        Only the filled cells need to be validated according to the mentioned rules.
+//
+//
+//        Example 1:
+//
+//
+//        Input: board =
+//        [["5","3",".",".","7",".",".",".","."]
+//        ,["6",".",".","1","9","5",".",".","."]
+//        ,[".","9","8",".",".",".",".","6","."]
+//        ,["8",".",".",".","6",".",".",".","3"]
+//        ,["4",".",".","8",".","3",".",".","1"]
+//        ,["7",".",".",".","2",".",".",".","6"]
+//        ,[".","6",".",".",".",".","2","8","."]
+//        ,[".",".",".","4","1","9",".",".","5"]
+//        ,[".",".",".",".","8",".",".","7","9"]]
+//        Output: true
+//        Example 2:
+//
+//        Input: board =
+//        [["8","3",".",".","7",".",".",".","."]
+//        ,["6",".",".","1","9","5",".",".","."]
+//        ,[".","9","8",".",".",".",".","6","."]
+//        ,["8",".",".",".","6",".",".",".","3"]
+//        ,["4",".",".","8",".","3",".",".","1"]
+//        ,["7",".",".",".","2",".",".",".","6"]
+//        ,[".","6",".",".",".",".","2","8","."]
+//        ,[".",".",".","4","1","9",".",".","5"]
+//        ,[".",".",".",".","8",".",".","7","9"]]
+//        Output: false
+//        Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+//
+//
+//        Constraints:
+//
+//        board.length == 9
+//        board[i].length == 9
+//        board[i][j] is a digit 1-9 or '.'.
+public class ValidSudoku {
+    class Solution {
+        public boolean isValidSudoku(char[][] board) {
+            int N = 9;
+            int r = N/3;
+            int l = N/3;
+            HashSet<Character> row[] = new HashSet[N];//存储行元素 的HashSet数组
+            HashSet<Character> col[] = new HashSet[N]; //存储列元素的 HashSet数组
+            HashSet<Character> box[][] = new HashSet[r][l];
+            for (int i = 0; i < N; i++) {
+                row[i] = new HashSet<Character>();
+                col[i] = new HashSet<Character>();
+            }
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < l; j++) {
+                    box[i][j] = new HashSet<Character>();//存储box元素的HashSet数组
+                }
+
+            }
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    char val = board[i][j];//此时遍历棋盘
+                    if(val == '.'){
+                        continue;
+                    }
+                    if(row[i].contains(val)){
+                        return false;
+                    }
+                    row[i].add(val);
+                    if(col[j].contains(val)){
+                        return false;
+                    }
+                    col[j].add(val);
+                    if(box[i/3][j/3].contains(val)){
+                        return false;
+                    }
+                    box[i/3][j/3].add(val);
+
+                }
+            }
+
+            return true;
+
+        }
+    }
+}
