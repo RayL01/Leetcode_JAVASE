50ce0d07c76512b26d8fd15d1d19f424e1b79fe5
Ray Liu <wooshadyyy@gmail.com>
2022-10-21 14:26:15 -0400

    Reverse the linked list, Time:O(n), Space: O(n), since the recursion calls add up on stack.

--- Leetcode/src/com/shady/MergeKSortedLists.java
+++ Leetcode/src/com/shady/MergeKSortedLists.java
@@ -1,2 +1,43 @@
-package com.shady;public class MergeKSortedLists {
+package com.shady;
+
+import java.util.PriorityQueue;
+
+/*You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+        Merge all the linked-lists into one sorted linked-list and return it.*/
+//[{1,4,5},{1,3,4},{2,6}]
+//Time complexity: O(Nlogk) while N is the number of the linked lists and k is the number of
+// all the nodes in the linked lists. Since each time we will pull out one element from
+//O(n) Creating a new linked list costs O(n)O(n) space.
+//O(k) The code above present applies in-place method which cost O(1) space.
+// And the priority queue (often implemented with heaps) costs O(k) space
+// (it's far less than NN in most situations).
+public class MergeKSortedLists {
+  class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+      ListNode dummy = new ListNode(0);
+      ListNode current = dummy;
+      PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->(a.val - b.val));
+
+      if(lists.length == 0){
+        return null;
+      }
+      //Put the head of each list into the priority queue
+      for(ListNode h: lists){
+        if(h!=null){//null pointer exception
+          pq.add(h);
+        }
+
+      }
+      while(!pq.isEmpty()) {
+        ListNode node = pq.poll();
+        current.next = node;
+        if (node.next != null) {
+          pq.add(node.next);
+        }
+        current = current.next;
+      }
+      return dummy.next;
+    }
+  }
 }
--- Leetcode/src/com/shady/ReverseLinkedList.java
+++ Leetcode/src/com/shady/ReverseLinkedList.java
@@ -1,2 +1,34 @@
-package com.shady;public class ReverseLinkedList {
+package com.shady;
+//Given the head of a singly linked list, reverse the list,
+// and return the reversed list.
+//
+//In this particular example, we're using recursion to implement the linked list reversing.
+//In order to do this, we need to reach the end of the linked list
+//Then how can we do this by recursion?
+//We need to call the reverse function until we reach the end of the list
+//
+//Complexity Analysis
+//
+//        Time complexity : O(n)O(n). Assume that nn is the list's length, the time complexity is O(n)O(n).
+//
+//        Space complexity : O(n)O(n). The extra space comes from implicit stack space due to recursion. The recursion could go up to nn levels deep.
+public class ReverseLinkedList {
+  class Solution {
+    public ListNode reverseList(ListNode head) {
+
+      //when the linked list is null, it returns null directly. Otherwise when the recursion reaches the
+      //last node in the linked list, it returns the node directly.
+      if(head == null || head.next == null){
+        return head;
+      }
+      // get the next node in the linked list
+      ListNode PrevNode = reverseList(head.next);
+      head.next.next = head;
+      head.next = null;
+
+      //In this case, we need to return the PrevNode which points to Node 5
+      // So that we get the head pointer in the end
+      return PrevNode;
+    }
+  }
 }
