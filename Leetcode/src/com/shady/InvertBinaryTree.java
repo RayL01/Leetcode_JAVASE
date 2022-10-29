5b4e2a6b0e5a43482ebdf377bea815734100f48c
Ray Liu <wooshadyyy@gmail.com>
2022-10-29 14:51:11 -0400

    invert the binary tree, time:O(n), space:O(n)

---
+++ Leetcode/src/com/shady/InvertBinaryTree.java
@@ -0,0 +1,26 @@
+package com.shady;
+
+
+
+//Given the root of a binary tree, invert the tree, and return its root.
+//TIme: O(n) Space: O(h) since h = O(n) , hence O(n)
+public class InvertBinaryTree {
+  class Solution {
+    public TreeNode invertTree(TreeNode root){
+      traverse(root);
+      return root;
+    }
+    public void traverse(TreeNode root) {
+      //Use pre-order traversing mechanism
+      if(root == null){
+        return;
+      }
+      TreeNode tmpNode = root.left;
+      root.left = root.right;
+      root.right = tmpNode;
+      traverse(root.left);
+      traverse(root.right);
+
+    }
+  }
+}
