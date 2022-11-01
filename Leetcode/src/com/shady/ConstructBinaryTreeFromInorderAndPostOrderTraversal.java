8c8b01191efdbffb4f7e48b9561b19a4c288b817
Ray Liu <wooshadyyy@gmail.com>
2022-11-01 12:06:26 -0400

    problem fixed

--- Leetcode/src/com/shady/ConstructBinaryTreeFromInorderAndPostOrderTraversal.java
+++ Leetcode/src/com/shady/ConstructBinaryTreeFromInorderAndPostOrderTraversal.java
@@ -1,33 +1,53 @@
 package com.shady;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class ConstructBinaryTreeFromInorderAndPostOrderTraversal {
-    Map<Integer, Integer> ValToIndex = new HashMap<>();
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
+  /**
+   * Definition for a binary tree node.
+   * public class TreeNode {
+   *     int val;
+   *     TreeNode left;
+   *     TreeNode right;
+   *     TreeNode() {}
+   *     TreeNode(int val) { this.val = val; }
+   *     TreeNode(int val, TreeNode left, TreeNode right) {
+   *         this.val = val;
+   *         this.left = left;
+   *         this.right = right;
+   *     }
+   * }
+   */
+  class Solution {
+    HashMap<Integer, Integer>ValToIndex = new HashMap<>();//store the mapping from value to index in the inorder array
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
       for (int i = 0; i < inorder.length; i++) {
         ValToIndex.put(inorder[i], i);
       }
-      return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+
+
+
+      return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
-    public TreeNode build(int [] inorder, int inLo, int inHi, int [] postorder, int poLo, int poHi){
-      if(inLo > inHi){
+    public TreeNode build(int preorder[], int prelo, int prehi, int inorder[], int inlo, int inhi){
+      //base case
+      if(prelo > prehi){
         return null;
       }
-      int root = postorder[poHi];
+
+      int root = preorder[prelo];
       int index = ValToIndex.get(root);
-      int leftsize = index - inLo; // get the left size;
+      int leftsize = index - inlo; // get the left size;
       //create the root node
-      int rightsize = inHi - index;
+
       TreeNode treeNode = new TreeNode(root);
-      treeNode.left = build(inorder, inLo, index - 1, postorder, poLo, poLo + leftsize -1);
-      treeNode.right = build(inorder, index + 1, inHi, postorder, poLo + leftsize, poHi -1);
-
-
+      treeNode.left = build(preorder, prelo + 1, prelo + leftsize, inorder, inlo, index - 1);
+      treeNode.right = build(preorder, prelo + leftsize + 1, prehi, inorder, index + 1, inhi);
+      //call the left and right recursion
 
 
 
       return treeNode;
     }
+  }
 }
