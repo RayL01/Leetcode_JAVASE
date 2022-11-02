9e6ea5fc3d2347b56a4c5b8d3879b625b69ecea8
Ray Liu <wooshadyyy@gmail.com>
2022-11-02 19:42:36 -0400

    Kth smallest element in  BST(recursion)

---
+++ Leetcode/src/com/shady/KthSmallestElementinaBST.java
@@ -0,0 +1,30 @@
+package com.shady;
+
+//Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+public class KthSmallestElementinaBST {
+  class Solution {
+    int rank = 0;//record the current rank of the node value.
+    int val = -1;// record the value of the node value
+    public int kthSmallest(TreeNode root, int k) {
+
+      traverse(root, k);
+      return val;
+
+    }
+    public void traverse(TreeNode root, int k){
+      if(root == null){
+        return;
+      }
+      traverse(root.left, k);//preorder
+
+      //inorder
+      if(++rank == k){
+        val = root.val;
+        return;
+      }
+
+      traverse(root.right, k);
+    }
+  }
+}
