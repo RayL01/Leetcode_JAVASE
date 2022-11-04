b905a7dc7ba53e718abd8b7531557011d4e35022
Ray Liu <wooshadyyy@gmail.com>
2022-11-04 00:41:52 -0400

    Validate binary search tree

---
+++ Leetcode/src/com/shady/ValidateBinarySearchTree.java
@@ -0,0 +1,21 @@
+package com.shady;
+
+public class ValidateBinarySearchTree {
+  class Solution {
+    public boolean isValidBST(TreeNode root) {
+      return Validate(root, null, null);
+    }
+    public boolean Validate(TreeNode root, TreeNode min, TreeNode max){//min means the minimal value of the current node, max means the maximal value of the current node
+      if(root == null){
+        return true;
+      }
+      if(min != null && root.val <= min.val){
+        return false;
+      }
+      if( max != null && root.val >= max.val){
+        return false;
+      }
+      return Validate(root.left, min, root)&&Validate(root.right, root, max);
+    }
+  }
+}
