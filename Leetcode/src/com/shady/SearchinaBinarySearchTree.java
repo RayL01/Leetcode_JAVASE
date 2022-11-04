4c90e13e86dbe9b3f485baa02f461da3a3953466
Ray Liu <wooshadyyy@gmail.com>
2022-11-04 00:56:13 -0400

    Search BST

---
+++ Leetcode/src/com/shady/SearchinaBinarySearchTree.java
@@ -0,0 +1,18 @@
+package com.shady;
+
+public class SearchinaBinarySearchTree {
+  class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+      if(root == null){
+        return null;
+      }
+      if(root.val < val){
+        searchBST(root.right, val);
+      }
+      if(root.val > val){
+        searchBST(root.left, val);
+      }
+      return root;
+    }
+  }
+}
