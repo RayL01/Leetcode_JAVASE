a331caa0d23fc61dd8a54d84faf85d5372337d2b
Ray Liu <wooshadyyy@gmail.com>
2022-11-11 15:17:29 -0500

    LCA for node with parent pointer

---
+++ Leetcode/src/com/shady/LowestCommonAncestorofaBinaryTreeIII.java
@@ -0,0 +1,33 @@
+package com.shady;
+
+import java.util.HashSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shadyyyyyl
+ * @Date: 2022/11/11/15:11
+ * @Description: FInd the LCA given that the Node has a parent pointer in its structure
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/
+ */
+public class LowestCommonAncestorofaBinaryTreeIII {
+  class Solution{
+    public Node lowestCommonAncestor(Node p, Node q){
+      HashSet<Node> set = new HashSet<>();
+      Node a = p;
+      Node b = q;
+      while(a != null){
+        set.add(a);
+        a = a.parent;
+      }
+
+      while(b != null){
+        if(set.contains(b)){
+          return b;
+        }
+        b = b.parent;
+      }
+      return null;
+    }
+  }
+}
---
+++ Leetcode/src/com/shady/Node.java
@@ -0,0 +1,15 @@
+package com.shady;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shadyyyyyl
+ * @Date: 2022/11/11/15:12
+ * @Description:
+ */
+public class Node {
+  public int val;
+  public Node left;
+  public Node right;
+  public Node parent;
+}
