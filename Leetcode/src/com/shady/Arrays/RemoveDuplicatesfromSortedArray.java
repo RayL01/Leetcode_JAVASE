26600fb4615f1f5ddfdce8bcd6f4367d6a67af53
Ray Liu <wooshadyyy@gmail.com>
2022-11-12 18:57:37 -0500

    remove duplicates from a sorted array using fast and slow pointers

---
+++ Leetcode/src/com/shady/Arrays/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,32 @@
+package com.shady.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shadyyyyyl
+ * @Date: 2022/11/12/18:44
+ * @Description: Delete duplicates in a sorted array and do not return a new array(modify the original array)
+ */
+public class RemoveDuplicatesfromSortedArray {
+  class Solution {
+    /**
+     *  0 1 2 3 4 5 6 7
+     *
+     *
+     *  0 0 1 1 2 3 3 4
+     * @param nums
+     * @return
+     */
+    //Using two pointers(slow and fast pointers)
+    public int removeDuplicates(int[] nums) {
+      int low = 0, high = 0;
+      while(high != nums.length){
+        if(nums[high] != nums[low]){
+          nums[++low] = nums[high];
+        }
+        high++;
+      }
+      return low + 1;
+    }
+  }
+}
