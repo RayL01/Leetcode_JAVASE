0ce7bb3e92846f437d91dfeb66896a5dddadaff5
Ray Liu <wooshadyyy@gmail.com>
2023-03-14 01:02:30 -0400

    Is Sequence solved by 2 pointers

---
+++ Leetcode/src/com/shady/TwoPointers/IsSubsequence.java
@@ -0,0 +1,23 @@
+package com.shady.TwoPointers;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shadyyyyyl
+ * @Date: 2023/03/14/00:50
+ * @Description:https://leetcode.com/problems/is-subsequence/description/
+ *
+ * Input: s = "abc", t = "ahbgdc"
+ * Output: true
+ */
+public class IsSubsequence {
+  public boolean isSubsequence(String s, String t) {
+    //Use two pointers
+    int j = 0;
+    for (int i = 0; i < t.length() && j < s.length(); i++) {
+      if(s.charAt(j) == t.charAt(i)) j++;
+    }
+    if(j == s.length()) return true;
+    return false;
+  }
+}
